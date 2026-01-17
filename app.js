94ec5d87bf821497a6eed161804ec0280f3d2a3d
Ubuntu <ubuntu@ip-172-31-2-146.ap-south-1.compute.internal>
2026-01-17 05:01:13 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('🚀 DevOps Pipeline Working!');
+});
+
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
